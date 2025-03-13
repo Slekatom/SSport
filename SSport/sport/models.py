e596b15ed60d1cf90642da827f1ff3f0c24b87e3
Slekatom <nemo.bma@gmail.com>
2025-03-13 21:40:44 +0200

    Model Training added

--- SSport/sport/models.py
+++ SSport/sport/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Training(models.Model):
+    date = models.DateField(default=date.today)
+    push_up = models.PositiveIntegerField(default=0)
+    squat = models.PositiveIntegerField(default=0)
+    press = models.PositiveIntegerField(default=0)
+    pull_up = models.PositiveIntegerField(default=0)
+    time_holding = models.PositiveIntegerField(default=1)
+
+    class Meta:
+        verbose_name = "Training"
+        verbose_name_plural = "Trainings"
+    
+    def __str__(self):
+        return f"""Training at {self.date}. You made {self.push_up} push-ups,
+        {self.squat} squat, {self.press} press, {self.pull_up} pull-ups,
+        holding time is {self.time_holding}"""
